a5987e51c0ef7a9235cac63c8bbe8aa5577846d3
MinGyoJung <jywlsh3@naver.com>
2019-12-20 01:41:15 +0900

    commit!

--- P_Board/src/com/Board/frontcontroller/BFrontController.java
+++ P_Board/src/com/Board/frontcontroller/BFrontController.java
@@ -9,47 +9,54 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.Board.Command.BCommand;
+
 /**
  * Servlet implementation class BFrontController
  */
-@WebServlet("*.do")	// URL 매핑을 확장자패턴, 확장자가 do면 무조건 이 컨트롤러로
+@WebServlet("*.do") // URL 매핑을 확장자패턴, 확장자가 do면 무조건 이 컨트롤러로
 public class BFrontController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public BFrontController() {
-        super();
-        // TODO Auto-generated constructor stub 
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public BFrontController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		System.out.println("doGet");
 		actionDo(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		System.out.println("doPost");
 		actionDo(request, response);
 	}
 
-	private void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	private void actionDo(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.setCharacterEncoding("EUC-KR");
-		
-		String viewPage = null;		// 어떤 뷰를 보여줄건지의 역할
-		BCommand command = null;	// 어떤 로직을 수행할건지 결정
-		
+
+		String viewPage = null; // 어떤 뷰를 보여줄건지의 역할
+		BCommand command = null; // 어떤 로직을 수행할건지 결정
+
 		String uri = request.getRequestURI();
 		String conPath = request.getContextPath();
 		String com = uri.substring(conPath.length());
-		
+
 		if (com.equals("/write_view.do")) {
 			viewPage = "write_view.jsp";
 		} else if (com.equals("/write.do")) {
@@ -68,7 +75,7 @@ public class BFrontController extends HttpServlet {
 			command = new BModifyCommand();
 			command.execute(request, response);
 			viewPage = "list.do";
-		} else if (com.equals("/delete.do")){
+		} else if (com.equals("/delete.do")) {
 			command = new BDeleteCommand();
 			command.execute(request, response);
 			viewPage = "list.do";
@@ -76,14 +83,14 @@ public class BFrontController extends HttpServlet {
 			command = new BReplyViewCommand();
 			command.execute(request, response);
 			viewPage = "reply_view.do";
-		} else if(com.equals("/reply.do")) {
+		} else if (com.equals("/reply.do")) {
 			command = new BReplyCommand();
 			command.execute(request, response);
 			viewPage = "list.do";
 		}
-		
+
 		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
 		dispatcher.forward(request, response);
+		// 포워딩
 	}
 }
-
