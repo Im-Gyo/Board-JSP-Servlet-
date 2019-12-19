37d86761017008966d7c65c7c954c300e9f326eb
MinGyoJung <jywlsh3@naver.com>
2019-12-20 01:01:55 +0900

    commit!

--- P_Board/src/com/Board/frontcontroller/BFrontController.java
+++ P_Board/src/com/Board/frontcontroller/BFrontController.java
@@ -1,6 +1,8 @@
 package com.Board.frontcontroller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class BFrontController
  */
-@WebServlet("/BFrontController")
+@WebServlet("*.do")	// URL 매핑을 확장자패턴, 확장자가 do면 무조건 이 컨트롤러로
 public class BFrontController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -19,23 +21,69 @@ public class BFrontController extends HttpServlet {
      */
     public BFrontController() {
         super();
-        // TODO Auto-generated constructor stub
+        // TODO Auto-generated constructor stub 
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("doGet");
+		actionDo(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		System.out.println("doPost");
+		actionDo(request, response);
 	}
 
+	private void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("EUC-KR");
+		
+		String viewPage = null;		// 어떤 뷰를 보여줄건지의 역할
+		BCommand command = null;	// 어떤 로직을 수행할건지 결정
+		
+		String uri = request.getRequestURI();
+		String conPath = request.getContextPath();
+		String com = uri.substring(conPath.length());
+		
+		if (com.equals("/write_view.do")) {
+			viewPage = "write_view.jsp";
+		} else if (com.equals("/write.do")) {
+			command = new BWriteCommand();
+			command.execute(request, response);
+			viewPage = "list.do";
+		} else if (com.equals("/list.do")) {
+			command = new BListCommand();
+			command.execute(request, response);
+			viewPage = "list.jsp";
+		} else if (com.equals("/content_view.do")) {
+			command = new BContentCommand();
+			command.execute(request, response);
+			viewPage = "content_view.jsp";
+		} else if (com.equals("/modify.do")) {
+			command = new BModifyCommand();
+			command.execute(request, response);
+			viewPage = "list.do";
+		} else if (com.equals("/delete.do")){
+			command = new BDeleteCommand();
+			command.execute(request, response);
+			viewPage = "list.do";
+		} else if (com.equals("reply_view.do")) {
+			command = new BReplyViewCommand();
+			command.execute(request, response);
+			viewPage = "reply_view.do";
+		} else if(com.equals("/reply.do")) {
+			command = new BReplyCommand();
+			command.execute(request, response);
+			viewPage = "list.do";
+		}
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
+		dispatcher.forward(request, response);
+	}
 }
+
